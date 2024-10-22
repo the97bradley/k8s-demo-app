fa42cfc1a03dd466659a78f2407946fac8298e8d
Bradley Schwartz <bradley.schwartz@datadoghq.com>
2024-10-22 16:27:29 -0400

    update proj name
    fixed proj id
    
    tf refresh
    
    proj name still incorrect
    
    rename bucket to fix conflict
    
    rename bucket again
    
    run refresh
    
    refresh instead of apply
    
    explicitly set gcloud creds

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "wiz-cluster"
+  name     = "host-cluster"
   location = "us-central1-a"
 
   initial_node_count = 3  # Ensure cluster has nodes
@@ -13,11 +13,22 @@ resource "google_container_cluster" "primary" {
   node_config {
     machine_type = "e2-medium"
   }
+
+  # Prevent cluster destruction and recreation
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 
-resource "google_storage_bucket" "mongo-backups" {
-  name          = "mongo-backup-bucket"
+resource "google_storage_bucket" "mongo-backups-bucket-k8s-project" {
+  name          = "mongo-backups-bucket"
   location      = "US"
   uniform_bucket_level_access = true
+
+
+  # Prevent bucket destruction and recreation
+  lifecycle {
+    prevent_destroy = true
+  }
 }
