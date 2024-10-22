c19534da398cc6ee0ab80742e73a6b6cfc7fdbda
Bradley Schwartz <bradley.schwartz@datadoghq.com>
2024-10-22 15:42:49 -0400

    forgot another var

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "project_id" {
   description = "Google Cloud Project ID"
   type        = string
 }
+
+
+variable "gcp_credentials" {
+  description = "GCP auth credentials"
+  type        = string
+} 
