eab8b922feab92d7a5431033cea27471ffa9db22
Bradley Schwartz <bradley.schwartz@datadoghq.com>
2024-10-31 22:19:37 -0600

    switch to resource based
    bucket naming
    
    bucket naming
    
    might I be retarded?
    
    idiiotic apple zip file
    
    zip format

--- infra/main.tf
+++ infra/main.tf
@@ -14,19 +14,23 @@ terraform {
 }
 
 
+
 resource "google_cloudfunctions_function" "mongo_backup_function" {
   name        = "mongo_backup_function"
   runtime     = "python39"
   entry_point = "backup_mongo"
   trigger_http = true
   source_archive_bucket =  "backup-function-code"
-  source_archive_object = "backup-func.zip"
+  source_archive_object =  "backup-func.zip"
+
 
   environment_variables = {
-    GOOGLE_FUNCTION_SOURCE = "main.py"  # Explicitly set the source if needed
+    GOOGLE_FUNCTION_SOURCE = "backup-func/main.py" 
   }
 
   lifecycle {
     prevent_destroy = true
   }
 }
+
+
